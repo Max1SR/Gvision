3db66bc3bbf595fc42049fb0d3c5d1e1dbea68cf
max1sr <msanchezr2107@alumno.ipn.mx>
2026-06-08 21:51:31 -0600

    feat: Add Validation with JWT and Encrypt with Bcrypt

---
+++ backend/middleware/auth.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+//El middleware auth.js es un cadenero que se para frente a la puerta de cada ruta 
+// y revisa si la peticion de React tien JWT o es usuario Invitado
+
+const verificarToken = (req, res, next) => {
+  //Extraemos el token de los encabezados
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  // NO TOKEN = INVITADO
+  if (!token) {
+    req.esInvitado = true; 
+    return next(); 
+  }
+
+  // Si hay token, verificamos que no sea falso o esté caducado
+  try {
+    const decodificado = jwt.verify(token, "MI_CLAVE_SECRETA_SUPER_SEGURA_SISISI_123");
+    req.usuario = decodificado; 
+    req.esInvitado = false;
+    next(); 
+  } catch (error) {
+    //Si el token es inválido o caducó: 
+    return res
+      .status(403)
+      .json({ error: "Tu sesión ha expirado o el token es inválido." });
+  }
+};
+
+export default verificarToken;
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Usuario from "../models/Usuario.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -43,6 +44,52 @@ router.post("/registro", async (req, res) => {
     // Cualquier otro error del servidor
     console.error("Error en registro:", error);
     res.status(500).json({ error: "Ocurrió un error interno en el servidor." });
+  }
+});
+
+// Ruta POST para Iniciar Sesión
+// Endpoint: http://localhost:3000/api/usuarios/login
+router.post('/login', async (req, res) => {
+  try {
+    const { correo, password } = req.body;
+
+    if (!correo || !password) {
+      return res.status(400).json({ error: 'Proporciona correo y contraseña.' });
+    }
+
+    // buscar usuario en la base de datos
+    const usuario = await Usuario.findOne({ where: { correo: correo } });
+    if (!usuario) {
+      return res.status(401).json({ error: 'Credenciales incorrectas.' });
+    }
+
+    // Verificamos que la contraseña coincida
+    const passwordValida = await usuario.validarPassword(password);
+    if (!passwordValida) {
+      return res.status(401).json({ error: 'Credenciales incorrectas.' });
+    }
+
+
+    // Creacion de JWT: ID + CLAVE + VIDAJWT
+    const token = jwt.sign(
+      { id: usuario.id, correo: usuario.correo }, 
+      'MI_CLAVE_SECRETA_SUPER_SEGURA_SISISI_123', 
+      { expiresIn: '24h' }
+    );
+
+    // LE DECIMOS AL FRONT QUE TODO BIEN
+    res.status(200).json({
+      mensaje: 'Inicio de sesión exitoso',
+      token: token,
+      usuario: {
+        id: usuario.id,
+        correo: usuario.correo
+      }
+    });
+
+  } catch (error) {
+    console.error('Error en login:', error);
+    res.status(500).json({ error: 'Ocurrió un error en el servidor.' });
   }
 });
 
